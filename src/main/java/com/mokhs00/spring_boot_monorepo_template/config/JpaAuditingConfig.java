a1d518b43e25bf55b883c1db24b9b1219352111f
mokhs00 <mokhs00@naver.com>
2021-12-09 15:33:10 +0900

    add : JpaAuditingConfig, QueryDslConfig

---
+++ src/main/java/com/mokhs00/spring_boot_monorepo_template/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.mokhs00.spring_boot_monorepo_template.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
---
+++ src/main/java/com/mokhs00/spring_boot_monorepo_template/config/QueryDslConfig.java
@@ -0,0 +1,21 @@
+package com.mokhs00.spring_boot_monorepo_template.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Configuration
+public class QueryDslConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+
+}
