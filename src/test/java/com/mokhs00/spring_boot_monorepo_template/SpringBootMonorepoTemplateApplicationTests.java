7e8e6c23754353f5a83193aba04477353d9e4370
mokhs00 <mokhs00@naver.com>
2021-12-09 15:24:28 +0900

    init : project

---
+++ src/test/java/com/mokhs00/spring_boot_monorepo_template/SpringBootMonorepoTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mokhs00.spring_boot_monorepo_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMonorepoTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
