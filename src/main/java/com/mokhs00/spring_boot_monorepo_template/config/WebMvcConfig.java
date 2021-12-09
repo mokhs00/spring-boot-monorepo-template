dc62d673e914b63f2078dabaa9f67cdaa8435b12
mokhs00 <mokhs00@naver.com>
2021-12-09 15:33:37 +0900

    add : WebMvcConfig

---
+++ src/main/java/com/mokhs00/spring_boot_monorepo_template/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.mokhs00.spring_boot_monorepo_template.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .maxAge(3600);
+    }
+}
