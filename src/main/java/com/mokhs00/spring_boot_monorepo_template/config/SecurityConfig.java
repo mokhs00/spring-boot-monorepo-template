4805137f9b9503182bae0c653ad04182e3e89897
mokhs00 <mokhs00@naver.com>
2022-01-25 21:02:22 +0900

    fix : cors config

--- src/main/java/com/mokhs00/spring_boot_monorepo_template/config/SecurityConfig.java
+++ src/main/java/com/mokhs00/spring_boot_monorepo_template/config/SecurityConfig.java
@@ -55,7 +55,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .httpBasic().disable()
-                .cors().disable()
+                .cors()
+                .and()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
